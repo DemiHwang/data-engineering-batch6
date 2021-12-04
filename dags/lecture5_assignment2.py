a94a247b8f8e10966fdbd8e232c0dd71276ee0d9
demi <demi@grepp.co>
2021-12-04 21:06:59 +0900

    PK의 uniquness를 보장하기 위해 temp table 만드는 방식 사용함

--- dags/lecture5_assignment2.py
+++ dags/lecture5_assignment2.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.models import Variable
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.amazon.aws.operators.redshift import RedshiftSQLOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 import requests
@@ -44,7 +45,7 @@ def load(json_array):
         temperature = elem['temp']['day']
         min_temperature = elem['temp']['min']
         max_temperature = elem['temp']['max']
-        sql += f"INSERT INTO demi.weather_forecast VALUES ('{date}', {temperature}, {min_temperature}, {max_temperature});"
+        sql += f"INSERT INTO demi.temp_weather_forecast VALUES ('{date}', {temperature}, {min_temperature}, {max_temperature});"
     sql += "END;"
     cur.execute(sql)
     logging.info(sql)
@@ -64,7 +65,45 @@ default_args = {
 }
 
 with DAG(DAG_ID, default_args=default_args, schedule_interval=None) as dag:
-    task = PythonOperator(
-        task_id = 'perform_etl',
-        python_callable = etl,
+    create_and_replicate_table = RedshiftSQLOperator(
+        task_id='create_and_replicate_table',
+        sql=[
+            """
+            CREATE TABLE IF NOT EXISTS demi.weather_forecast (
+                date date PRIMARY KEY,
+                temperature double precision,
+                min_temperature double precision,
+                max_temperature double precision,
+                created_date timestamp without time zone DEFAULT getdate()
+            );
+            """,
+            "DROP TABLE IF EXISTS demi.temp_weather_forecast;",
+            "CREATE TABLE demi.temp_weather_forecast AS SELECT * FROM demi.weather_forecast;",
+        ]
     )
+
+    weathermap_to_redshift = PythonOperator(
+        task_id='perform_etl',
+        python_callable=etl,
+    )
+
+    drop_duplicates = RedshiftSQLOperator(
+        task_id='drop_duplicates',
+        sql=[
+            "BEGIN;",
+            "DELETE FROM demi.weather_forecast;",
+            """
+            INSERT INTO demi.weather_forecast
+            SELECT date, temperature, min_temperature, max_temperature, created_date
+            FROM (
+                SELECT
+                    *,
+                    ROW_NUMBER() OVER (PARTITION BY date ORDER BY created_date DESC) seq
+                FROM demi.temp_weather_forecast)
+                WHERE seq = 1;
+            END
+            """,
+        ]
+    )
+
+    replicate_table >> weathermap_to_redshift >> drop_duplicates
